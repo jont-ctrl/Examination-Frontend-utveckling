3d844e9143df66f073abac3e6dc3003744c38cf6
gragas <wge3wtgdx2ret43eyhu5j13r3rt1t@users.noreply.github.com>
2024-12-20 23:47:01 +0100

    added light dark mode

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hello');
+const darkMode = document.querySelector('#darkMode');
+
+console.log(darkMode);
+console.log(darkMode.textContent);
+
+darkMode.addEventListener('click', () => {
+  if (document.body.classList.contains('dark-mode')) {
+    document.body.classList.remove('dark-mode');
+    darkMode.textContent = 'dark_mode';
+  } else {
+    document.body.classList.add('dark-mode');
+    darkMode.textContent = 'light_mode';
+  }
+});
