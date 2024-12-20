5a47ed74dee6c5e4532b7daa2555d3e23f8d9bea
gragas <wge3wtgdx2ret43eyhu5j13r3rt1t@users.noreply.github.com>
2024-12-21 00:54:44 +0100

    added fetch 10 movies

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const darkMode = document.querySelector('#darkMode');
-
-console.log(darkMode);
-console.log(darkMode.textContent);
+const main = document.querySelector('main');
+const movieArea = document.querySelector('.movieArea');
 
 darkMode.addEventListener('click', () => {
   if (document.body.classList.contains('dark-mode')) {
@@ -12,3 +11,52 @@ darkMode.addEventListener('click', () => {
     darkMode.textContent = 'light_mode';
   }
 });
+
+let movies = [];
+let favorites = [];
+console.log(movies);
+// Api fetch
+const apiKi = 'b43ebaaf';
+
+async function getMovies() {
+  try {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKi}&s=movie`
+    );
+    const data = await response.json();
+    console.log(data);
+
+    data.Search.forEach((element) => {
+      const newDiv = document.createElement('div');
+      newDiv.classList.add('movieCard');
+
+      const newMovieTitle = document.createElement('h2');
+      newMovieTitle.classList.add('movieTitle');
+      newMovieTitle.textContent = element.Title;
+
+      const newImagePoster = document.createElement('img');
+      newImagePoster.classList.add('moviePoster');
+      newImagePoster.src = element.Poster;
+
+      const newReadMoreBtn = document.createElement('button');
+      newReadMoreBtn.textContent = 'Läs mer';
+
+      newDiv.append(newMovieTitle, newImagePoster, newReadMoreBtn);
+      movieArea.append(newDiv);
+    });
+
+    /*     const newDiv = document.createElement('div');
+    newDiv.classList.add('movieCard');
+
+    const newMovieTitle = document.createElement('h2');
+    newMovieTitle.classList.add('movieTitle');
+    newMovieTitle.textContent = data.Search[0].Title;
+
+    newDiv.append(newMovieTitle);
+    main.append(newDiv); */
+  } catch (error) {
+    console.error('Error occured: ', error);
+  }
+}
+
+getMovies();
